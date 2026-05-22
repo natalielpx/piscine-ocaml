d5a7491c75622956999042612ce4b59370444e31
natalie <natalielpx@gmail.com>
2026-05-22 21:49:04 +0200

    [01-04] ft: implements tail recursion

---
+++ md01/ex04/fibonacci.ml
@@ -0,0 +1,26 @@
+(* Allowed functions : None *)
+let fibonacci n =
+  let rec aux acc n = 
+    if n < 0 then
+      -1
+    else if n = 0 then
+      acc
+    else if n = 1 then
+      acc + 1
+    else
+      (aux acc (n - 2)) + (aux acc (n - 1));
+  in
+  aux 0 n
+
+(** Demonstration Program *)
+let () =
+  print_int (fibonacci (-42));
+  print_newline();
+  print_int (fibonacci 1);
+  print_newline();
+  print_int (fibonacci 3);
+  print_newline();
+  print_int (fibonacci 6);
+  print_newline();
+  print_int (fibonacci 20);
+  print_newline()
