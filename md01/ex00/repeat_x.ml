10fb5b53d6d7b00d363cace1ec6121154685a668
natalie <natalielpx@gmail.com>
2026-05-22 20:42:32 +0200

    [01-00] ft: concatenates strings

---
+++ md01/ex00/repeat_x.ml
@@ -0,0 +1,16 @@
+(* Allowed functions : None *)
+let rec repeat_x n =
+  if n < 0 then
+    "Error"
+  else if n = 0 then
+    ""
+  else
+    "x" ^ repeat_x(n - 1)
+
+(** Demonstration Program *)
+let () =
+  print_endline (repeat_x (-1));
+  print_endline (repeat_x 0);
+  print_endline (repeat_x 1);
+  print_endline (repeat_x 2);
+  print_endline (repeat_x 42);
