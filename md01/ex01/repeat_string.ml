0426de2a8c3bbafeddfd294c21eb2faa9a1e95c8
natalie <natalielpx@gmail.com>
2026-05-22 20:51:01 +0200

    [01-01] ft: handles optional arguments

---
+++ md01/ex01/repeat_string.ml
@@ -0,0 +1,16 @@
+(* Allowed functions : None *)
+let rec repeat_string ?(str = "x") n =
+  if n < 0 then
+    "Error"
+  else if n = 0 then
+    ""
+  else
+    str ^ repeat_string ~str (n - 1)
+
+(** Demonstration Program *)
+let () =
+  print_endline (repeat_string (-1));
+  print_endline (repeat_string 0);
+  print_endline (repeat_string ~str:"Toto" 1);
+  print_endline (repeat_string 2);
+  print_endline (repeat_string ~str:"what" 10)
