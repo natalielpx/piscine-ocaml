62391aafb9d2135df62e1e17f9e211a037e115b2
natalie <natalielpx@gmail.com>
2026-05-22 18:48:22 +0200

    [00-03] ft: functions prints entire alphabet

---
+++ md00/ex03/ft_print_alphabet.ml
@@ -0,0 +1,10 @@
+(* Allowed functions : char_of_int, int_of_char, print_char *)
+
+let rec ft_print_next_alphabet i =
+  print_char (char_of_int i);
+  if i < int_of_char 'z' then
+    ft_print_next_alphabet (i + 1)
+
+let ft_print_alphabet () =
+  ft_print_next_alphabet (int_of_char 'a');
+  print_char '\n'
