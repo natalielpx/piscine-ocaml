309df7bc5b363481cdcef9c2a5c98df9018cf086
natalie <natalielpx@gmail.com>
2026-05-25 12:41:05 +0200

    [02-02] ft: combines recursion with algorithms

---
+++ md02/ex02/gray.ml
@@ -0,0 +1,47 @@
+(* Allowed functions : String module and not the @ operator *)
+
+let rec ft_power x y =
+  if x < 0 || y < 0 then  -1
+  else if y = 0     then  1   (* 0 to the power of 0 is hereby defined as 1*)
+  else if x = 0     then  0
+  else                    x * ft_power x (y - 1)
+
+let get_target index num =
+  let rec aux n = 
+    if (index mod (ft_power 2 (n - 1))) = 0  then num - n
+    else                                          aux (n - 1)
+  in
+  aux num
+
+let gray_code prev index number =
+  let target = get_target (index + 1) number in
+  let rec aux next i =
+    if i = number then
+      next ^ " "
+    else
+      let bit = 
+        if i = target then 
+          if prev.[i] = '0' then '1' else '0'
+        else
+          prev.[i]
+      in
+      aux (next ^ (String.make 1 bit)) (i + 1)
+  in
+  aux "" 0
+
+let gray number =
+  let index = (ft_power 2 number) - 1 in
+  let rec aux code i =
+    print_string code;
+    if i < index then aux (gray_code code i number) (i + 1)
+  in
+  aux (String.make number '0' ^ " ") 0;
+  print_newline ()
+
+(* Demonstration Program *)
+
+let () =
+  gray 1;
+  gray 2;
+  gray 3;
+  gray 4
