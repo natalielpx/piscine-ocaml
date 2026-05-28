da1ab5939506d3300fc0c760c6ceb5773cf50241
nlam <nlam@f1br8s17.paris.42.school>
2026-05-28 16:32:54 +0200

    [01] REF: changes alginments

--- md01/ex02/ackermann.ml
+++ md01/ex02/ackermann.ml
@@ -1,13 +1,9 @@
 (* Allowed functions : None *)
 let rec ackermann m n =
-  if m < 0 || n < 0 then
-    -1
-  else if m = 0 then
-    n + 1
-  else if n = 0 then
-    ackermann (m - 1) 1
-  else
-    ackermann (m - 1) (ackermann m (n - 1))
+  if m < 0 || n < 0 then  -1
+  else if m = 0     then  n + 1
+  else if n = 0     then  ackermann (m - 1) 1
+  else                    ackermann (m - 1) (ackermann m (n - 1))
 
 (** Demonstration Program *)
 let () =
