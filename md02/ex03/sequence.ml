00988f68accb100aee9885c7b9b7651898dcca58
natalie <natalielpx@gmail.com>
2026-05-29 17:34:00 +0200

    [02-03] ref: rewrites functions

--- md02/ex03/sequence.ml
+++ md02/ex03/sequence.ml
@@ -1,40 +1,42 @@
 (* Allowed functions : None *)
 
-let ltos list =
-  let rec aux str lst =
-    match lst with
-    | [] -> str
-    | l :: tail -> aux (str ^ l) tail
+let rev lst=
+  let rec aux acc = function
+    | []        -> acc
+    | l :: tail -> aux (l :: acc) tail
   in
-  aux "" list
+  aux [] lst
 
-let itoa_pos number =
-  let hex = [|"0"; "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"|] in
-  let rec aux res num =
-    if num < 10 then  hex.(num) ^ res
-    else              aux (hex.(num mod 10) ^ res) (num / 10)
+let ltos lst =
+  let rec aux str = function
+    | []        -> str
+    | l :: tail -> aux (l ^ str) tail
   in
-  aux "" number
-  
+  aux "" (rev lst)
+
+(* Maximum Consecutive Run Count is at most 3
+  A run of 4 would require the prior term
+  to have had a count of 11
+  (two consecutive digits forming 11),
+  which itself would have been collapsed.
+  By induction,
+  starting from 1,
+  no run longer than 3 ever appears. *)
 let get_next_sequence prev =
-  let rec aux next count el tail =
-    match tail with
-    | [] -> (next @ [(itoa_pos count); el])
-    | e :: t ->
-      if e = el then
-        aux next (count + 1) e t
-      else
-        aux (next @ [(itoa_pos count); el]) 1 e t
+  let rec aux next = function
+    | []                                    -> rev next
+    | a :: b :: c :: t  when a = b && b = c -> aux (a :: "3" :: next) t
+    | a :: b :: t       when a = b          -> aux (a :: "2" :: next) t
+    | a :: t                                -> aux (a :: "1" :: next) t
   in
-  match prev with
-  | [] -> []
-  | elem :: rest -> aux [] 1 elem rest
+  aux [] prev  
 
 let sequence number =
   if number < 1 then raise (Invalid_argument "number must be larger than 1");
   let rec aux res n =
-    if n = number then  ltos res
-    else                aux (get_next_sequence res) (n + 1)
+    if n = number
+    then ltos res
+    else aux (get_next_sequence res) (n + 1)
   in
   aux ["1"] 1
 
