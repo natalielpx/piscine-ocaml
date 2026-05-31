2486ead79c89548695471d547587c58b89840530
nlam <nlam@f1br7s10.paris.42.school>
2026-05-31 16:48:03 +0200

    [01-04] fix: properly implements tail recursion

--- md01/ex04/fibonacci.ml
+++ md01/ex04/fibonacci.ml
@@ -1,22 +1,25 @@
 (* Allowed functions : None *)
-let fibonacci n =
-  let rec aux acc n = 
-    if      n < 0 then  -1
-    else if n = 0 then  acc
-    else if n = 1 then  acc + 1
-    else                (aux acc (n - 2)) + (aux acc (n - 1));
-  in
-  aux 0 n
+let fibonacci number =
+  if      number < 0 then -1
+  else if number = 0 then 0
+  else if number = 1 then 1
+  else
+    let rec aux acc1 acc2 n =
+      if n = number
+      then acc1 + acc2
+      else aux acc2 (acc1 + acc2) (n + 1)
+    in
+    aux 0 1 2
 
 (** Demonstration Program *)
 let () =
   print_int (fibonacci (-42));
   print_newline();
-  print_int (fibonacci 1);
+  print_int (fibonacci 0);
   print_newline();
-  print_int (fibonacci 3);
+  print_int (fibonacci 1);
   print_newline();
   print_int (fibonacci 6);
   print_newline();
-  print_int (fibonacci 20);
+  print_int (fibonacci 80); (* F80 = 2.3416728348468E+16 *)
   print_newline()
