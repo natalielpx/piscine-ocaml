62ca530e0966395236afd139f3623b762634cfdb
natalie <natalielpx@gmail.com>
2026-06-05 17:02:22 +0200

    [04-06] ft: handles more complex parsing

---
+++ md04/ex06/examples_of_file.ml
@@ -0,0 +1,58 @@
+(* Forbidden functions : None *)
+
+(** Write a function named examples_of_file that
+  * takes a file path as its argument and 
+  * returns a set of examples read from the input file,
+  * formatted as CSV *)
+let examples_of_file path =
+
+  (* Open file *)
+  let in_channel = 
+    try open_in path
+    with e -> failwith "Unable to open file"
+  in
+
+  (* Read lines *)
+  let lines =
+    let rec aux acc =
+      match input_line in_channel with
+      | line                  -> aux (line :: acc)
+      | exception End_of_file -> close_in in_channel; acc
+      | exception e           -> close_in_noerr in_channel; failwith "Unexpected error occured"
+    in
+    aux []
+  in
+
+  (* Parse lines (list of strings) into lists of lists of strings *)
+  let split = 
+    let rec aux acc = function
+    | []            -> acc
+    | head :: tail  -> aux (((String.split_on_char ',' head)) :: acc) tail
+    in
+    aux [] lines
+  in
+
+  (* Transform split (lists of lists of strings) into arrays of (float array * string) tuples *)
+  let radars =
+    let func lst = 
+      let arr = Array.of_list lst in
+      let len = Array.length arr in
+      (Array.map float_of_string (Array.sub arr 0 (len - 1)), arr.(len - 1))
+    in
+    Array.map func (Array.of_list split)
+  in
+
+  (* Return result *)
+  radars
+
+(** Demonstration Program *)
+let () =
+  let arr = examples_of_file "./ionosphere.test.csv" in
+  let print_radar (flts, cls) =
+    for i = 0 to Array.length flts - 1 do
+      print_float flts.(i);
+      print_string ","
+    done;
+    print_endline cls;
+  in
+  Array.iter print_radar arr
